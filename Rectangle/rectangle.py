77be36753595d00230b7abb6506b7e8df692fb59
artemiy-228 <artemsch17@gmail.com>
2023-06-07 23:47:51 +0100

    Create method display()

--- Rectangle/rectangle.py
+++ Rectangle/rectangle.py
@@ -1,11 +1,17 @@
-class Rectangle():
+class Rectangle:
     def __init__(self, length=2, width=2):
         self.length = length
         self.width = width
+        self.area_v = self.area()
 
-    def Calc_Perimetеr(self):
+    def perimetеr(self):
         return (self.length + self.width) * 2
 
-    def Calc_Area(self):
+    def area(self):
         return self.width * self.length
 
+    def display(self):
+        print(f"Length of rectangle is {self.length}")
+        print(f"Width of rectangle is {self.width}")
+        print(f"Perimeter of rectangle is {self.perimetеr()}")
+        print(f"Area of rectangle is {self.area_v}")
