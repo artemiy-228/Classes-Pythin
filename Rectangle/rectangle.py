37b9e26dd0218c496e86be4ded4f33c3c758d39b
artemiy-228 <artemsch17@gmail.com>
2023-06-07 22:39:49 +0100

    Create methods to calculate Area and Perimeter

--- Rectangle/rectangle.py
+++ Rectangle/rectangle.py
@@ -1,12 +1,11 @@
 class Rectangle():
-
     def __init__(self, length=2, width=2):
         self.length = length
         self.width = width
 
     def Calc_Perimetеr(self):
-        print(f"Perimetеr of Rectangle is {(self.length + self.width) * 2} units")
+        return (self.length + self.width) * 2
 
     def Calc_Area(self):
-        print(f"Area of Rectangle is {self.width * self.length} square units")
+        return self.width * self.length
 
