886a97dfcf9b57e55e8336ba5aa9a2ef8925554d
artemiy-228 <artemsch17@gmail.com>
2023-06-08 17:49:43 +0100

    Create test_belongs and show how does it work

--- circle.py
+++ circle.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import pi, sqrt
 
 
 class Circle:
@@ -8,8 +8,27 @@ class Circle:
         self.y = y
         self.radius = radius
 
+
     def area(self):
         return pi * self.radius ** 2
 
+
     def perimeter(self):
         return pi * 2 * self.radius
+
+
+    def test_belongs(self, a, b):
+        distance = sqrt((self.x - a)**2 + (self.y - b)**2)
+        return distance <= self.radius
+
+
+    def display(self):
+        print(f"Circle radius is {self.radius}")
+        print(f"Circle area is {self.area()}")
+        print(f"Circle perimeter is {self.perimeter()}")
+
+
+circle = Circle(0, 0, 10)
+circle.display()
+print(circle.test_belongs(5, 5))
+print(circle.test_belongs(10, 10))
