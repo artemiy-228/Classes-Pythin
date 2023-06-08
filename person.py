470f8b837418b860870c4edb63592b7e59e8874a
artemiy-228 <artemsch17@gmail.com>
2023-06-08 16:55:21 +0100

    Add display method

--- person.py
+++ person.py
@@ -1,6 +1,10 @@
 class Person:
 
-    def __init__(self, age=18, name="Student"):
+    def __init__(self, name="Student", age=18):
         self.age = age
         self.name = name
+
+
+    def display(self):
+        print(f"Student {self.name} is {self.age} years old")
 
