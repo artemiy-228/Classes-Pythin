3630d4b6d0e4afc4ca81571c9c28c85abbea19ec
artemiy-228 <artemsch17@gmail.com>
2023-06-08 17:40:16 +0100

    Create area and perimeter methods

--- circle.py
+++ circle.py
@@ -1,3 +1,6 @@
+from math import pi
+
+
 class Circle:
 
     def __init__(self, x, y, radius):
@@ -5,4 +8,8 @@ class Circle:
         self.y = y
         self.radius = radius
 
-    
+    def area(self):
+        return pi * self.radius ** 2
+
+    def perimeter(self):
+        return pi * 2 * self.radius
