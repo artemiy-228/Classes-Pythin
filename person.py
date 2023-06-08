e16a553a46977b3a4928000487b2d9db2898b307
artemiy-228 <artemsch17@gmail.com>
2023-06-08 17:00:23 +0100

    Create Child class

--- person.py
+++ person.py
@@ -1,10 +1,17 @@
 class Person:
 
-    def __init__(self, name="Student", age=18):
+    def __init__(self, name="Person", age=30):
         self.age = age
         self.name = name
 
 
     def display(self):
         print(f"Student {self.name} is {self.age} years old")
+
+
+class Student(Person):
+
+    def __init__(self, name="Student", age=18, major="Math"):
+        Person.__init__(self, name, age)
+        self.major = major
 
