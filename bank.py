b754c02a7207b39ac87f3e053aa3ceea3e6d8116
artemiy-228 <artemsch17@gmail.com>
2023-06-08 17:33:48 +0100

    Work version

--- bank.py
+++ bank.py
@@ -18,6 +18,21 @@ class BankAccount:
         self.balance *= 0.95
 
     def display(self):
+        print("<" + "=" * 25 + ">")
         print(f"Account id is {self.account_number}")
         print(f"Owners name is {self.name}")
         print(f"Account balance is {self.balance}")
+        print("<" + "=" * 25 + ">")
+
+
+id55665 = BankAccount(55665, "Artemy", 0)
+id55666 = BankAccount(55666, "Vildan", 0)
+id55667 = BankAccount(55667, "Nikita", 0)
+
+id55665.deposit(4000)
+id55666.deposit(13000)
+id55667.deposit(5000)
+
+id55665.display()
+id55666.display()
+id55667.display()
