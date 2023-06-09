22a58096f4dffd0cd3452603e550d4b0ccb78eab
artemiy-228 <artemsch17@gmail.com>
2023-06-09 19:11:47 +0100

    test_prims

--- Computation.py
+++ Computation.py
@@ -1,4 +1,5 @@
 class Computation:
+
     def __init__(self):
         pass
 
@@ -22,8 +23,19 @@ class Computation:
             d += 1
         return True
 
+    def test_prims(self, a, b):
+        while a != 0 and b != 0:
+            if a > b:
+                a %= b
+            else:
+                b %= a
+        if a + b == 1:
+            return True
+        return False
+
 
 Calculator = Computation()
 print(Calculator.factorial(5))
 print(Calculator.sum(5))
 print(Calculator.test_prime(111))
+print(Calculator.test_prims(121, 11))
