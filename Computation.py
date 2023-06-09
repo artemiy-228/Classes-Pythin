7cf4b146adfe60f4c856575d5fcd2ef4fbf455b4
artemiy-228 <artemsch17@gmail.com>
2023-06-09 19:00:28 +0100

    Create factorial method

--- Computation.py
+++ Computation.py
@@ -1,4 +1,11 @@
 class Computation:
     def __init__(self):
         pass
+
+    def factorial(self, num):
+        f = 1
+        for i in range(1, num + 1):
+            f *= i
+        return f
+
     
