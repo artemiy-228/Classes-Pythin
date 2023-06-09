fc60ca759a7ff2fc0bd97da907ae4adb1581df73
artemiy-228 <artemsch17@gmail.com>
2023-06-09 19:07:17 +0100

    Create test_prime method

--- Computation.py
+++ Computation.py
@@ -11,7 +11,19 @@ class Computation:
     def sum(self, num):
         return int(num * (num + 1) / 2)
 
+    def test_prime(self, num):
+        num = abs(num)
+        if num <= 1:
+            return False
+        d = 2
+        while(d * d <= num):
+            if num % d == 0:
+                return False
+            d += 1
+        return True
+
 
 Calculator = Computation()
 print(Calculator.factorial(5))
 print(Calculator.sum(5))
+print(Calculator.test_prime(111))
